9186ae617ce3665d030e5d01296ade7161116df2
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2023-10-28 21:52:27 +0300

    add chris

---
+++ pages/chris.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex justify-center m-auto">
+        <img class="w-64 h-64" src="../assets/chris.jpg" alt="chris">
+    </div>
+</template>
--- pages/nuxt-anime.vue
+++ pages/nuxt-anime.vue
@@ -5,6 +5,8 @@
   </template>
 
 <script setup lang="ts">
+import { useAnime } from '#imports';
+
 onMounted(() => {
     useAnime({ targets: '.title', translateX: 250, duration: 800 })
 })
