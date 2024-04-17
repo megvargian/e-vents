9399525a3c7e654b53cedc1bf29aba6dfecdd50b
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-04-17 19:59:51 +0300

    add favicon and fix laptop size

--- app.vue
+++ app.vue
@@ -3,6 +3,9 @@
 </template>
 
 <script setup lang="ts">
+useHead({
+  link: [{ rel: 'icon', type: 'image/png', href: './public/favicon.png' }]
+});
 onBeforeMount(() => {
   const documentHeight = () => {
     document.documentElement.style.setProperty(
